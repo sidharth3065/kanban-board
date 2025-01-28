aa55364bf6c9ada56f441335be89b438e9b59869
sidharth singh <siddarth3065@gmail.com>
2025-01-28 16:04:29 +0530

    fix: update vercel config and env vars

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,25 @@ import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css'
 import App from './App.jsx'
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <Auth0Provider
-    domain="dev-sw1d7l7t2qqh8vlw.us.auth0.com"
-    clientId="fMSoCSIh5zOzm2kmDEfoFAcZEZ4IV5mF"
-     authorizationParams={{
-    redirect_uri: process.env.NODE_ENV === 'production' 
-      ? 'https://your-vercel-app.vercel.app' 
-      : 'http://localhost:5173'
-  }}
-  >
-    <App />
-  </Auth0Provider>,
-  </StrictMode>,
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: 
+          import.meta.env.DEV 
+            ? 'http://localhost:5173/api/auth/callback' 
+            : 'https://kanban-board.vercel.app/api/auth/callback'
+      }}
+      logoutParams={{
+        returnTo: 
+          import.meta.env.DEV 
+            ? 'http://localhost:5173' 
+            : 'https://kanban-board.vercel.app'
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </StrictMode>
 )
